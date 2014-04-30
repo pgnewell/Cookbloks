70a03eadb6309056b18b4028b2d99c7a97bdeb19
Paul Newell <paulgnewell@gmail.com>
2014-04-30 17:52:27 -0400

    correct ordering for steps

--- lib/CookBloks/Schema/Result/Recipe.pm
+++ lib/CookBloks/Schema/Result/Recipe.pm
@@ -167,7 +167,7 @@ __PACKAGE__->has_many(
   "steps",
   "CookBloks::Schema::Result::Step",
   { "foreign.recipe" => "self.id" },
-  { cascade_copy => 0, cascade_delete => 1 },
+  { cascade_copy => 0, cascade_delete => 1, order_by => \'step ASC'},
 );
 
 =head2 user
